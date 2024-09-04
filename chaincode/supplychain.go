27d44a2c3228e30ba0c5e375d7d4bc1c0c45620f
Dayal Mukati <dayalmukati@gmail.com>
2024-09-04 21:45:01 +0530

    Question Updated

--- chaincode/supplychain.go
+++ chaincode/supplychain.go
@@ -19,60 +19,15 @@ type Product struct {
 }
 
 func (s *SupplyChainContract) CreateProduct(ctx contractapi.TransactionContextInterface, productID string, productName string, owner string) error {
-	product := Product{
-		ProductID:   productID,
-		ProductName: productName,
-		Owner:       owner,
-		Status:      "created",
-	}
-
-	productAsBytes, err := json.Marshal(product)
-	if err != nil {
-		return fmt.Errorf("failed to marshal product: %s", err.Error())
-	}
-
-	return ctx.GetStub().PutState(productID, productAsBytes)
+	// Write the logic to create the product
 }
 
 func (s *SupplyChainContract) TransferProduct(ctx contractapi.TransactionContextInterface, productID string, newOwner string) error {
-	productAsBytes, err := ctx.GetStub().GetState(productID)
-	if err != nil {
-		return fmt.Errorf("failed to get product: %s", err.Error())
-	} else if productAsBytes == nil {
-		return fmt.Errorf("product %s does not exist", productID)
-	}
-
-	product := new(Product)
-	err = json.Unmarshal(productAsBytes, product)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal product: %s", err.Error())
-	}
-
-	product.Owner = newOwner
-
-	productAsBytes, err = json.Marshal(product)
-	if err != nil {
-		return fmt.Errorf("failed to marshal product: %s", err.Error())
-	}
-
-	return ctx.GetStub().PutState(productID, productAsBytes)
+	// Write the logic to transfer the product
 }
 
 func (s *SupplyChainContract) GetProduct(ctx contractapi.TransactionContextInterface, productID string) (*Product, error) {
-	productAsBytes, err := ctx.GetStub().GetState(productID)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get product: %s", err.Error())
-	} else if productAsBytes == nil {
-		return nil, fmt.Errorf("product %s does not exist", productID)
-	}
-
-	product := new(Product)
-	err = json.Unmarshal(productAsBytes, product)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal product: %s", err.Error())
-	}
-
-	return product, nil
+	// Write the logic to get the product
 }
 
 func main() {
